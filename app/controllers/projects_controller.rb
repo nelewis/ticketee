21536426f36548084ef2b1bb365256bbcebd2b4a
Nicole Lewis <lewis.nicole@gmail.com>
2013-01-22 00:16:00 -0800

    getting closer

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,16 @@
+class ProjectsController < ApplicationController
+	def index
+	end
+	def new
+		@project = Project.new
+	end
+	def create
+		@project = Project.new(params[:project]) <co id="ch03_551_1"/>
+		if @project.save
+			flash[:notice] = "Project has been created."
+			redirect_to @project
+		else
+			# nothing, yet
+		end
+	end 
+end
