695b9bfd59abe4758abfc95ee2f967152aee0959
Nicole Lewis <lewis.nicole@gmail.com>
2013-01-28 22:04:50 -0800

    User can view a list of projects

--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 feature 'Creating Projects' do
 	scenario "can create a project" do
 		visit '/'
---
+++ spec/support/project_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :project do
+	name "Example project" #<co id="ch04_v2_4_1"/>
+  end
+end
