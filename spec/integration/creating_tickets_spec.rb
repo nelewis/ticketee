3c4a5d92161a5ad40901e5a7cd8893f19fb9fa1a
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-04 22:49:07 -0800

    allowing regression errors in createing_tickets_spec.rb

--- spec/integration/creating_tickets_spec.rb
+++ spec/integration/creating_tickets_spec.rb
@@ -19,13 +19,13 @@ feature "Creating Tickets" do
 	page.should have_content("Project has been created.")
   end
   
-  # scenario "Creating a ticket without valid attributes fails" do
-	# #click_button "Create Ticket"
-	# click_button "Create Project"
-	# page.should have_content("Ticket has not been created.")
-	# page.should have_content("Title can't be blank")
-	# page.should have_content("Description can't be blank")
-  # end
+  scenario "Creating a ticket without valid attributes fails" do
+	#click_button "Create Ticket"
+	click_button "Create Project"
+	page.should have_content("Ticket has not been created.")
+	page.should have_content("Title can't be blank")
+	page.should have_content("Description can't be blank")
+  end
   
   scenario "Description must be longer than 10 characters in nested ticket inside project" do
   	fill_in "Name", :with => "Non-standards compliance"
@@ -35,7 +35,7 @@ feature "Creating Tickets" do
 	fill_in "Title", :with => "Non-standards compliance"
 	fill_in "Description", :with => "it sucks"
 	click_button "Create Ticket"
-	page.should have_content("Ticket has not been created.")
-	#page.should have_content("Description is too short")
+	page.should have_content("Ticket has not been created.") # doesn't go away 
+	page.should have_content("Description is too short") # doesn't show
   end
 end
