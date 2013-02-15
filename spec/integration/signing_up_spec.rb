ad4f03fa487e1724cbb797bd08a7fc6ec380e436
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-14 21:05:48 -0800

    fixing integration error with signing in via a confirmation email using email_spec

--- spec/integration/signing_up_spec.rb
+++ spec/integration/signing_up_spec.rb
@@ -8,6 +8,7 @@ feature 'Signing up' do
    fill_in "Password", :with => "password"
    fill_in "Password confirmation", :with => "password"
    click_button "Sign up"
-   page.should have_content("You have signed up successfully.")
+   #page.should have_content("You have signed up successfully.")
+   page.should have_content("A message with a confirmation link has been sent to your email address. Please open the link to activate your account.")
   end
 end
