4346114989f1fd508c4ac5983c8c3cf47ddf583b
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-15 17:14:42 -0800

    fixing integration error on displaying who signed in

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,13 +1,18 @@
 class TicketsController < ApplicationController
+before_filter :authenticate_user!, :except => [:index, :show]
 before_filter :find_project
 before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+
   def new
     @ticket = @project.tickets.build
   end
+
   def show
   end
+
   def create
     @ticket = @project.tickets.build(params[:ticket])
+    @ticket.user = current_user
     if @ticket.save
 	  flash[:notice] = "Ticket has been created."
 	  redirect_to [@project, @ticket]
@@ -16,8 +21,10 @@ before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 	  render :action => "new"
     end
   end
+
   def edit
   end
+
   def update
 	if @ticket.update_attributes(params[:ticket])
 		flash[:notice] = "Ticket has been updated."
@@ -27,11 +34,13 @@ before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 		render :action => "edit"
 	end
   end
+
   def destroy
 	@ticket.destroy
 	flash[:notice] = "Ticket has been deleted."
 	redirect_to @project
   end
+
   private
 	def find_project
 		@project = Project.find(params[:project_id])
