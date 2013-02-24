ed58c9c2c0db25875f0b2d96d2a364815d1611ec
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-23 22:53:04 -0800

    hiding links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ module ApplicationHelper
 	def title(*parts) 
 		unless parts.empty?
 			content_for :title do
-				(parts << "Ticketee").join(" - ")
+				(parts << "Ticketee").join(" - ") unless parts.empty?
 			end
 		end
+	end
+	
+	def admins_only(&block)
+	  yield if current_user.try(:admin?)
 	end
 end
---
+++ spec/support/capybara_helpers.rb
@@ -0,0 +1,20 @@
+module CapybaraHelpers
+  def assert_no_link_for(text)
+    page.should_not(have_css("a", :text => text),
+      "Expected to not see the #{text.inspect} link, but did.")
+  end
+
+  def assert_link_for(text)
+    page.should(have_css("a", :text => text),
+      "Expected to see the #{text.inspect} link, but did not.")
+  end
+
+  # def state_line_for(state)
+    # state = State.find_by_name!(state)
+    # "#state_#{state.id}"
+  # end
+end
+
+RSpec.configure do |config|
+  config.include CapybaraHelpers, :type => :request
+end
