c67cb2a75f67b46d2569c216706028a29cbf6458
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-24 23:24:09 -0800

    Adding polymorphic permissions

--- spec/integration/viewing_projects_spec.rb
+++ spec/integration/viewing_projects_spec.rb
@@ -1,16 +1,21 @@
-require 'spec_helper'
-
-feature "Viewing projects:" do
-  before do
-	textmate_2 = Factory(:project, :name => "TextMate 2")
-		
-	internet_explorer = Factory(:project, :name => "Internet Explorer")
-
-	visit '/'
-  end
-  
-  scenario "Listing all projects" do
-	page.should have_content("TextMate 2")
-	page.should have_content("Internet Explorer")
-  end
-end
+require 'spec_helper'
+
+feature "Viewing projects:" do
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:project) { Factory(:project) }
+
+  before do
+    sign_in_as!(user)
+    define_permission!(user, :view, project)
+    #textmate_2 = Factory(:project, :name => "TextMate 2")
+    #internet_explorer = Factory(:project, :name => "Internet Explorer")
+  end
+
+  scenario "Listing all projects" do
+    visit '/'
+    click_link project.name
+    page.current_url.should == project_url(project)
+    #page.should have_content("TextMate 2")
+    #page.should have_content("Internet Explorer")
+  end
+end
