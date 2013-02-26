5969860c11f05fc7c8948f1f244f65b3fa6cc596
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-25 19:41:04 -0800

    adding User delete for an admin, with the safeguard you cannot delete yourself

--- spec/integration/admin/deleting_users_spec.rb
+++ spec/integration/admin/deleting_users_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-feature 'Deleting users' do
+feature 'Deleting users:' do
   let!(:admin_user) { Factory(:admin_user) }
   let!(:user) { Factory(:user) }
   before do
@@ -12,5 +12,10 @@ feature 'Deleting users' do
     click_link user.email
     click_link "Delete User"
     page.should have_content("User has been deleted")
+  end
+  scenario "Users cannot delete themselves" do
+    click_link admin_user.email
+    click_link "Delete User"
+    page.should have_content("You cannot delete yourself!")
   end
 end
