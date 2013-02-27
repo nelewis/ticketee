9b8a0c24356baffd33e58affbfbb6ed02efe33ec
Nicole Lewis <lewis.nicole@gmail.com>
2013-02-26 16:32:49 -0800

    adding AuthZ. Created a seeds.rb so I can reset development as needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+admin_user = User.create(:email => "admin@gmail.com",
+                         :password => "password")
+admin_user.admin = true
+admin_user.confirm!
+admin_user2 = User.create(:email => "user@gmail.com",
+                          :password => "password")
+admin_user2.admin = false
+admin_user2.confirm!
+
+
+Project.create([
+                 {:name =>"Seed 1st project", :description=>"Seed 1st projects description"}
+])
+Ticket.create ([
+                 {:title =>"Seed 1st ticket", :description=>"Seed 1st projects ticket description", :project_id=>1, :user_id=>1}
+])
--- spec/integration/viewing_projects_spec.rb
+++ spec/integration/viewing_projects_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
-
 feature "Viewing projects:" do
   let!(:user) { Factory(:confirmed_user) }
   let!(:project) { Factory(:project) }
-
   before do
     sign_in_as!(user)
     define_permission!(user, :view, project)
-    #textmate_2 = Factory(:project, :name => "TextMate 2")
-    #internet_explorer = Factory(:project, :name => "Internet Explorer")
   end
-
   scenario "Listing all projects" do
     visit '/'
     click_link project.name
