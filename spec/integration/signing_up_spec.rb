af76ce0320f2b36268fb099c01a74e769d10a460
Nicole Lewis <lewis.nicole@gmail.com>
2013-03-02 17:02:05 -0800

    adding controllers for registrations

--- spec/integration/signing_up_spec.rb
+++ spec/integration/signing_up_spec.rb
@@ -1,14 +1,15 @@
-require 'spec_helper'
-
-feature 'Signing up' do
- scenario 'Successful sign up' do
-   visit '/'
-   click_link 'Sign up'
-   fill_in "Email", :with => "user@ticketee.com"
-   fill_in "Password", :with => "password"
-   fill_in "Password confirmation", :with => "password"
-   click_button "Sign up"
-   #page.should have_content("You have signed up successfully.")
-   page.should have_content("A message with a confirmation link has been sent to your email address. Please open the link to activate your account.")
-  end
-end
+require 'spec_helper'
+
+feature 'Signing up' do
+  scenario 'Successful sign up' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in "Email", :with => "user@ticketee.com"
+    fill_in "Password", :with => "password"
+    fill_in "Password confirmation", :with => "password"
+    click_button "Sign up"
+    #page.should have_content("You have signed up successfully.")
+    #page.should have_content("A message with a confirmation link has been sent to your email address. Please open the link to activate your account.")
+    page.should have_content("Please open the link to activate your account.")
+  end
+end
