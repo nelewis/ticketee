248fd9f40da253c4d6fd3448789669d802eea9bd
Nicole Lewis <lewis.nicole@gmail.com>
2013-03-04 11:55:45 -0800

    ensuring new tickets and new projects links are controlled

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,55 +1,61 @@
-class TicketsController < ApplicationController
-  #before_filter :authenticate_user!, :except => [:index, :show]
-  before_filter :authenticate_user!
-  before_filter :find_project
-  before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
-
-  def new
-    @ticket = @project.tickets.build
-  end
-
-  def show
-  end
-
-  def create
-    @ticket = @project.tickets.build(params[:ticket])
-    @ticket.user = current_user
-    if @ticket.save
-      flash[:notice] = "Ticket has been created."
-      redirect_to [@project, @ticket]
-    else
-      flash[:alert] = "Ticket has not been created."
-      render :action => "new"
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @ticket.update_attributes(params[:ticket])
-      flash[:notice] = "Ticket has been updated."
-      redirect_to [@project, @ticket]
-    else
-      flash[:alert] = "Ticket has not been updated."
-      render :action => "edit"
-    end
-  end
-
-  def destroy
-    @ticket.destroy
-    flash[:notice] = "Ticket has been deleted."
-    redirect_to @project
-  end
-
-  private
-  def find_project
-    @project = Project.for(current_user).find(params[:project_id])
-  rescue ActiveRecord::RecordNotFound
-    flash[:alert] = "The project you were looking for could not be found."
-    redirect_to root_path
-  end
-  def find_ticket
-    @ticket = @project.tickets.find(params[:id])
-  end
-end
+class TicketsController < ApplicationController
+  #before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :authenticate_user!
+  before_filter :find_project
+  before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
+
+  def new
+    @ticket = @project.tickets.build
+  end
+
+  def show
+  end
+
+  def create
+    @ticket = @project.tickets.build(params[:ticket])
+    @ticket.user = current_user
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been created."
+      render :action => "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @ticket.update_attributes(params[:ticket])
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
+  def authorize_delete!
+    if !current_user.admin? && cannot?(:"delete tickets", @project)
+      flash[:alert] = "You cannot delete tickets from this project."
+      redirect_to @project
+    end
+  end
+
+  private
+  def find_project
+    @project = Project.for(current_user).find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to root_path
+  end
+  def find_ticket
+    @ticket = @project.tickets.find(params[:id])
+  end
+end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,16 +1,55 @@
-require 'spec_helper'
-describe TicketsController do
-  let(:user) { Factory(:confirmed_user) }
-  let(:project) { Factory(:project) }
-  let(:ticket) { Factory(:ticket, :project => project,
-                         :user => user) }
-  context "standard users" do
-    it "cannot access a ticket for a project" do
-      sign_in(:user, user)
-      get :show, :id => ticket.id, :project_id => project.id
-      response.should redirect_to(root_path)
-      flash[:alert].should eql("The project you were looking " +
-                               "for could not be found.")
-    end
-  end
-end
+require 'spec_helper'
+describe TicketsController do
+  let(:user) { Factory(:confirmed_user) }
+  let(:project) { Factory(:project) }
+  let(:ticket) { Factory(:ticket, :project => project,
+                         :user => user) }
+  context "standard users" do
+    it "cannot access a ticket for a project" do
+      sign_in(:user, user)
+      get :show, :id => ticket.id, :project_id => project.id
+      response.should redirect_to(root_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+  end
+  context "with permission to view the project" do
+    before do
+      sign_in(:user, user)
+      define_permission!(user, "view", project)
+    end
+
+    def cannot_create_tickets!
+      response.should redirect_to(project)
+      flash[:alert].should eql("You cannot create tickets on this project.")
+    end
+    def cannot_update_tickets!
+      response.should redirect_to(project)
+      flash[:alert].should eql("You cannot edit tickets on this project.")
+    end
+    it "cannot begin to create a ticket" do
+      get :new, :project_id => project.id
+      cannot_create_tickets!
+    end
+    it "cannot create a ticket without permission" do
+      post :create, :project_id => project.id
+      cannot_create_tickets!
+    end
+    it "cannot edit a ticket without permission" do
+      get :edit, { :project_id => project.id, :id => ticket.id }
+      cannot_update_tickets!
+    end
+    it "cannot update a ticket without permission" do
+      put :update, { :project_id => project.id,
+                     :id => ticket.id,
+                     :ticket => {}
+                     }
+      cannot_update_tickets!
+    end
+    it "cannot delete a ticket without permission" do
+      delete :destroy, { :project_id => project.id, :id => ticket.id }
+      response.should redirect_to(project)
+      message = "You cannot delete tickets from this project."
+      flash[:alert].should eql(message)
+    end
+  end
+end
