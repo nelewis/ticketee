4d44730ee7936b474a91d5f1e0c85c974f5cd63b
Nicole Lewis <lewis.nicole@gmail.com>
2013-03-05 00:15:14 -0800

    Add the ability to attach a file to a ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  attr_accessible :description, :title
+  attr_accessible :title, :description, :asset
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
+  has_attached_file :asset
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,25 @@
 Ticketee::Application.routes.draw do
-  #get "/admin/users/index"
-
-
   devise_for :users, :controllers => { :registrations => "registrations" }
+
+  root :to => "projects#index"
+  resources :projects do
+    resources :tickets
+  end
+
+  resources :files
+
+  namespace :admin do
+    root :to => "base#index"
+    resources :users do
+      resources :permissions
+    end
+  end
 
   get '/awaiting_confirmation',
     :to => "users#confirmation",
     :as => 'confirm_user'
 
-  root :to => "projects#index"
-
-  resources :projects do
-
-    resources :tickets
-
-  end
-
-  namespace :admin do
-
-    root :to=> "base#index"
-
-    resources :users
-
-    resources :permissions
-
-  end
-
   put '/admin/users/:user_id/permissions',
-
-
-
     :to => 'admin/permissions#update',
-
-
-
     :as => :update_user_permissions
-
 end
